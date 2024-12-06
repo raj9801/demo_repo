f5bfbf107366db5417fb19b7f718940f99cc5c10
raj9801 <119560097+raj9801@users.noreply.github.com>
2024-12-06 14:39:54 -0600

    Update App.js
    Initial Commit

--- demo_repo/App.js
+++ demo_repo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello this my demo app</Text>
+      <Text>Hello this my Demo App</Text>
       <StatusBar style="auto" />
     </View>
   );
